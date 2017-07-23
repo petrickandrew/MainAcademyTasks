d547caf5a04a369e9d2fb824c3233c44c69a3ebb
Petrick <petrickandrew@gmail.com>
2017-07-24 00:19:48 +0300

    first home task print Strings var on screen

---
+++ src/com/MainAcademy/Petrick/HomeTask.java
@@ -0,0 +1,25 @@
+package com.MainAcademy.Petrick;
+
+/*
+Домашнее задание №1
+студента Петрик Андрея petrickandrew@gmail.com
+по курсу "Атоматизированного тестирования" Main Academy
+проверяющий Генадий Мирошниченко.
+
+Задание:
+  Объявить переменную name типа String.
+ Сразу же в строке объявления присвоить ей какое-нибудь значение.
+ Выведи на экран переменную name.
+
+ */
+public class HomeTask {
+    public static void main(String[] args) {
+        String name = "Поехали!", gagarin="       Юрий Гагарин.", str = "\u2122" ;
+
+        System.out.println(name);
+        System.out.print(gagarin);
+        System.out.print(str);
+    }
+}
+
+
