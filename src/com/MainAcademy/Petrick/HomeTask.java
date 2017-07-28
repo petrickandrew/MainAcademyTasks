1cba5f34508ea36841b81a6507644e81de111a2a
Petrick <petrickandrew@gmail.com>
2017-07-28 23:51:32 +0300

    Home task #3

--- src/com/MainAcademy/Petrick/HomeTask.java
+++ src/com/MainAcademy/Petrick/HomeTask.java
@@ -40,6 +40,7 @@ public class HomeTask {
 3. Программа должна считывать данные с клавиатуры.
 4. Программа должна выводить квадрат считанного числа.
 */
+/*
 import java.util.Scanner;
 
 public class HomeTask {
@@ -64,5 +65,46 @@ public class HomeTask {
 
         }
 }
+*/
+//-------------------------------------------------------------------------------------------------
+/*
+Домашнее задание №3
+студента Петрик Андрея petrickandrew@gmail.com
+по курсу "Атоматизированного тестирования" Main Academy
+проверяющий Генадий Мирошниченко.
 
+Задание:
+расставить правильно знаки плюс и минус, чтобы значение переменной result
+получилось равным 20.
+
+public class Solution {
+public static int a = 1;
+public static int b = 3;
+public static int c = 9;
+public static int d = 27;
+public static void main(String[] args) {
+                                   int result = + a + b + c + d;
+                                    System.out.println(result);
+                                       }
+                      }
+Требования:
+1. Значения переменных: a, b, c, d не изменяй.
+2. Перед каждой из переменных: a, b, c, d в строке с объявлением
+переменной result должен стоять один знак плюс либо минус.
+3. В результате работы программы на экран нужно вывести число 20.
+4. Знаки плюс и минус должны быть расставлены правильно
+*/
+
+public class HomeTask {
+    public static int a = 1;
+    public static int b = 3;
+    public static int c = 9;
+    public static int d = 27;
+    public static void main(String[] args) {
+
+        int result = -a + b - c + d;
+        System.out.println(result);
+    }
+}
+//----------------------------------------------------------------------------
 
