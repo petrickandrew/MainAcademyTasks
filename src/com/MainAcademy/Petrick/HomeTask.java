caaff28443d7b053ca85bc9d5996190753bdb720
Petrick <petrickandrew@gmail.com>
2017-07-29 00:14:04 +0300

    Home task #4

--- src/com/MainAcademy/Petrick/HomeTask.java
+++ src/com/MainAcademy/Petrick/HomeTask.java
@@ -94,7 +94,7 @@ public static void main(String[] args) {
 3. В результате работы программы на экран нужно вывести число 20.
 4. Знаки плюс и минус должны быть расставлены правильно
 */
-
+/*
 public class HomeTask {
     public static int a = 1;
     public static int b = 3;
@@ -106,5 +106,50 @@ public class HomeTask {
         System.out.println(result);
     }
 }
+*/
 //----------------------------------------------------------------------------
+/*
+Домашнее задание №4
+студента Петрик Андрея petrickandrew@gmail.com
+по курсу "Атоматизированного тестирования" Main Academy
+проверяющий Генадий Мирошниченко.
 
+Задание:
+Закомментировать часть кода, чтобы на экран вывелось сообщение «summa =
+        12»
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3;
+        int a = 10;
+        int b = 6;
+        int b = 12;
+        int summa = 1 + a + b;
+        int summa = 2 + a + b;
+        int summa = 3 + a + b;
+        int summa = 4 + a + b;
+        System.out.println("summa = " + summa);
+    }
+}
+Требования:
+        1. Программа должна выводить на экран текст.
+        2. Нужно закомментировать необходимые строки и не менять остальные.
+        3. В программу нельзя добавлять новые строки или удалять имеющиеся.
+        4. Выведенный текст должен соответствовать заданию.
+*/
+
+public class HomeTask {
+
+    public static void main(String[] args) {
+        int a = 3;
+       // int a = 10;
+        int b = 6;
+      //  int b = 12;
+       // int summa = 1 + a + b;
+       // int summa = 2 + a + b;
+        int summa = 3 + a + b;
+       // int summa = 4 + a + b;
+        System.out.println("summa = " + summa);
+    }
+}
+
+//---------------------------------------------------------------------------------
